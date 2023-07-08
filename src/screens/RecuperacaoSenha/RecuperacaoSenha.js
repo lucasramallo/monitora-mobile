de3630b1511578bae1595b28aad8d75436f57ac4
Raykkoner Dujhkkovick Silva de Farias <raykkonerd@gmail.com>
2023-07-08 12:46:30 -0300

    update src/screens/RecuperacaoSenha/RecuperacaoSenha.js

---
+++ src/screens/RecuperacaoSenha/RecuperacaoSenha.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, View, TouchableOpacity, Text, Alert } from 'react-native';
+import { primaryColor, secondaryColor} from '../assets/colors/index';
+
+export default function RecuperacaoSenha(){
+  const [mensagem, setMensagem] = useState('Digite o endereço de email previamente cadastrado por você. Enviaremos um email com o código de autenticação.');
+  const [email, setEmail] = useState('');
+
+
+  const adicionaDominio = () => {
+    setEmail(email + '@academico.ifpb.edu.br')
+  }
+
+  const envia = () => 0;
+  
+  return (
+    <View style={styles.tela}>
+      <Text style={styles.titulo}>Recuperação de Senha</Text>
+      <View style={styles.viewMensagem}>
+        <Text style={styles.mensagem}>
+          {mensagem}
+        </Text>
+      </View>
+      <TextInput 
+        placeholder="Email"
+        placeholderTextColor="#A3A3A3"
+        selectionColor="black"
+        style={styles.input}
+        value={email}
+        onChangeText={text => setEmail(text)}/>
+      
+      {(!email.includes('@') && email.length > 0) &&
+        <TouchableOpacity 
+          style={styles.atalho}
+          onPress={adicionaDominio}>
+          <Text>
+            @academico.ifpb.edu.br
+          </Text>
+        </TouchableOpacity>
+      }
+      
+      <TouchableOpacity style={styles.botao}>
+        <Text style={styles.botao.texto}>Enviar</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  back: {
+    alignSelf: "left"
+  },
+  tela: {
+    flex: 1,
+    alignItems: "center",
+    padding: 45,
+    backgroundColor: "white"
+  },
+  titulo: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 45
+  },
+  input: {
+    backgroundColor: secondaryColor,
+    borderWidth: 1,
+    borderColor: '#dcdcdc',
+    borderRadius: 12,
+    padding: 15,
+    marginTop: 20,
+    width: "100%",
+  },
+  atalho: {
+    padding: 3,
+    margin: 5,
+    borderWidth: 0.5,
+    borderColor: "black",
+    borderRadius: 12
+  },
+  botao: {
+    width: "100%",
+    backgroundColor: "#212226",
+    alignItems: "center",
+    borderRadius: 12,
+    padding: 15,
+    marginTop: 45,
+    texto: {
+      color: "white",
+      fontWeight: "bold",
+    },
+  },
+  viewMensagem: {
+    marginVertical: 25,
+  },
+  mensagem: {
+    color: "#333"
+  }
+});
