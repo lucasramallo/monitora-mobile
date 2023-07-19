7cc1f8a67ec662c4894a98d91be6dc2413b4a2e6
raykkoner <raykkonerd@gmail.com>
2023-07-18 22:16:23 -0300

    Feat(NewPassword): Validação de tela NewPassword

--- src/screens/NewPassword/NewPassword.js
+++ src/screens/NewPassword/NewPassword.js
@@ -10,7 +10,26 @@ export default function NewPassword(){
 	const [password, setPassword] = useState('');
 	const [passwordCopy, setPasswordCopy] = useState('');
 	const [showPassword, setShowPassword] = useState(true);
+	const [disableCopy, setDisableCopy] = useState(true);
+	const [strongness, setStrongness] = useState("No mínimo 8 caracteres")
 	const navigation = useNavigation();
+	
+	const handleValidation = (value) => {
+    if(value.length >= 8){
+			if(value.match(/[a-z]/g) && value.match(/[A-Z]/g) && value.match(/[0-9]/g) || value.match(/[a-zA-Z0-9]/g) && value.match(/[^a-zA-Z0-9]/g)){
+				setStrongness("Forte");
+			} else if(value.match(/[A-z]/g) && value.match(/[0-9]/g) || value.match(/[A-Z]/g) && value.match(/[a-z]/g)){
+				setStrongness("Segura");
+			} else {
+				setStrongness("Fraca");
+			}
+			setDisableCopy(false);
+		} else {
+			setStrongness("No mínimo 8 caracteres");
+			setDisableCopy(true);
+		}
+  };
+  
 	
 	const handleSubmit = () => {
 		navigation.navigate("Home");
@@ -22,48 +41,66 @@ export default function NewPassword(){
 				{"Redefinir senha"}
 			</Text>
 			
-			<Text style={styles.label}>
-        {"Nova Senha"}
-      </Text>
-      <View style={styles.input} >
-        <TextInput
-          placeholder="Digite sua nova senha"
-          placeholderTextColor="#A3A3A3"
-          selectionColor="black"
-          onChangeText={text => setPassword(text)}
-          value={password}
-          style={styles.passwordInput}
-          secureTextEntry={!showPassword}
-        />
-        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-          <Octicons 
-            name={showPassword ? "eye-closed" : "eye"} 
-            size={18} 
-            color="black" 
-            styles={styles.eyeIcon}/>
-        </TouchableOpacity>
+			<View style={styles.inputLabelView}>
+				<Text style={styles.label}>
+	        {"Nova Senha"}
+	      </Text>
+	      <View style={[
+	      	styles.input,
+	      	validation(strongness).input
+	      ]} >
+	        <TextInput
+	          placeholder="Digite sua nova senha"
+	          placeholderTextColor="#A3A3A3"
+	          selectionColor="black"
+	          onChangeText={(text) => {
+	          	setPassword(text);
+	          	handleValidation(text);
+	          }}
+	          value={password}
+	          style={styles.passwordInput}
+	          secureTextEntry={!showPassword}
+	        />
+	        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+	          <Octicons 
+	            name={showPassword ? "eye-closed" : "eye"} 
+	            size={18} 
+	            color="black" 
+	            styles={styles.eyeIcon}/>
+	        </TouchableOpacity>
+	      </View>
+	      <Text style={validation(strongness).warn}>{strongness}</Text>
       </View>
 			
-			<Text style={styles.label}>
-        {"De novo"}
-      </Text>
-      <View style={styles.input} >
-        <TextInput
-          placeholder="Digite de novo sua nova senha"
-          placeholderTextColor="#A3A3A3"
-          selectionColor="black"
-          onChangeText={text => setPasswordCopy(text)}
-          value={passwordCopy}
-          style={styles.passwordInput}
-          secureTextEntry={!showPassword}
-        />
-        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-          <Octicons 
-            name={showPassword ? "eye-closed" : "eye"} 
-            size={18} 
-            color="black" 
-            styles={styles.eyeIcon}/>
-        </TouchableOpacity>
+			<View style={styles.inputLabelView}>
+				<Text style={styles.label}>
+	        {"De novo"}
+	      </Text>
+	      <View style={[
+	      	styles.input,
+	      	disableCopy && validation().inputCopyDisabled
+	      ]} >
+	        <TextInput
+	          placeholder="Digite de novo sua nova senha"
+	          placeholderTextColor="#A3A3A3"
+	          selectionColor="black"
+	          editable={!disableCopy}
+	          onChangeText={(text) => {
+	          	setPasswordCopy(text);
+	          	setStrongness(null);
+	          }}
+	          value={passwordCopy}
+	          style={styles.passwordInput}
+	          secureTextEntry={!showPassword}
+	        />
+	        <TouchableOpacity disabled={disableCopy} onPress={() => setShowPassword(!showPassword)}>
+	          <Octicons 
+	            name={showPassword ? "eye-closed" : "eye"} 
+	            size={18} 
+	            color="black" 
+	            styles={styles.eyeIcon}/>
+	        </TouchableOpacity>
+	      </View>
       </View>
 			
 			<TouchableOpacity onPress={handleSubmit} style={styles.button}>
@@ -103,6 +140,8 @@ const styles = StyleSheet.create({
     borderColor: '#dcdcdc',
     borderRadius: 12,
     padding: 15,
+  },
+  inputLabelView: {
     marginBottom: 20,
   },
   button: {
@@ -118,3 +157,29 @@ const styles = StyleSheet.create({
     },
   },
 });
+
+const validation = passwordStrongness => {
+	const warnColors = {green: "green", orange: "orange", red: "#B00000"};
+	let validationColor = "#dcdcdc";
+	
+	if(passwordStrongness == "Forte"){
+		validationColor = warnColors.green;
+	} else if(passwordStrongness == "Segura"){
+		validationColor = warnColors.orange;
+	} else if(passwordStrongness == "Fraca" || passwordStrongness == "Senhas divergem!"){
+		validationColor = warnColors.red;
+	} 
+	
+	return StyleSheet.create({
+			input: {
+				borderColor: validationColor
+			}, 
+			inputCopyDisabled: {
+				backgroundColor: "#efefef"
+			},
+			warn: {
+				color: validationColor,
+				marginLeft: 12
+			}
+		});
+};
